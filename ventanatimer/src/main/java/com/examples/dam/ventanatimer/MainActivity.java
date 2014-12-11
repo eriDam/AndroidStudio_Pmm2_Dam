49a57a25da098e89cc8f5874a96bbc298132a4b1
eriDam <erika.c.caballero@gmail.com>
2014-12-11 16:46:26 +0100

    Finalizada app con Splash Screen Ocultados barra de titulo y menu.

--- ventanatimer/src/main/java/com/examples/dam/ventanatimer/MainActivity.java
+++ ventanatimer/src/main/java/com/examples/dam/ventanatimer/MainActivity.java
@@ -1,18 +1,19 @@
 package com.examples.dam.ventanatimer;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     /**
-     * Crearemos una constante para establecer un DELAY un retardo, en este caso para la
+     * Crearemos una constante para establecer un DELAY un retardo (la duración del
+     * splashScreen, en este caso para la
      * ejecución de 3000 milisegundos
      */
     private static final long RETARDO = 3000;
@@ -20,14 +21,19 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Definimos la orientacion a vertical - Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Escondemos la barra de Titulo - Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
         //Creo una tarea para lanzar después del DELAY(RETARDO)
         //Un timerTask es una tarea, una clase runable que se ejecuta en paralelo con el resto de tareas
         //y para que esto suceda tenemos que implementar el metodo run, lo que hará run es
-        //lanzar el mainActivity2
+
         TimerTask task = new TimerTask() {
             @Override
-            public void run() {
+            public void run() {//lanzar el mainActivity2
 
 
                 //arrancar la siguiente activity
@@ -35,7 +41,7 @@ public class MainActivity extends ActionBarActivity {
                         MainActivity.this, MainActivity2.class);
                 startActivity(mainIntent);
 
-                //Cerrar la actividad y el usuario puede volver atrás presionando el btn back
+                //Cerrar la actividad para que el usuario no pueda volver atrás presionando el btn back
                 finish();
             }
         };
@@ -47,25 +53,5 @@ public class MainActivity extends ActionBarActivity {
     timer.schedule(task,RETARDO);
 
 }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
