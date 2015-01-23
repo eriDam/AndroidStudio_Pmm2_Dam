b40043bb7e9670ad79a71ccf53f99b58487b7088
eriDam <erika.c.caballero@gmail.com>
2015-01-23 01:20:15 +0100

    Task 22. Music - MediaPlayer
    Play music with mediaPlayer, using and modify activity created to use
    soundPool.

--- musica/src/main/java/com/examples/dam/musica/MainActivity.java
+++ musica/src/main/java/com/examples/dam/musica/MainActivity.java
@@ -2,6 +2,7 @@ package com.examples.dam.musica;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,18 +12,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.media.AudioManager.OnAudioFocusChangeListener;
+import static android.media.MediaPlayer.OnPreparedListener;
 
+/**
+ * @author erika_000
+ *
+ * PROYECTO 5 - MENÚ DE ENTRADA - Reproducciendo música
+ * con MediaPlayer.
+ *
+ * 1.	Utilizando el Activity generado para la tarea con SoundPool
+ * 2.	Modificaremos las líneas necesarias para sustituir SoundPool por MediaPlayer
 
+ * */
 public class MainActivity extends Activity {
 
     //Control de volumen
     private int mVolume = 6, mVolumeMax = 10, mVolumeMin = 0;
-    //Control sonando
+    //Control sonando música
     private int sonando=0; //0=cancion no comenzada; 1=comenzada; 2=pause;
     private SoundPool mSoundPool;
+    //Creamos el MediaPlayer
+    private MediaPlayer mPlayer;
     private int mSoundId;
     private AudioManager mAudioManager;
     private boolean mCanPlayAudio;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +63,13 @@ public class MainActivity extends Activity {
                 if (mVolume < mVolumeMax) {
                     mVolume += 2;
                     tv.setText(String.valueOf(mVolume));
+                    //Cuando le demos a subir volumen, Le pasamos el volumen izqd y dcho, 2 canales y cada uno con un volumen
+                    mPlayer.setVolume((float) mVolume / mVolumeMax, (float)mVolume / mVolumeMax);
                 }
             }
         });
 
-        // Bajar Volumne
+        // Bajar Volumen de la canción
         final Button downButton = (Button) findViewById(R.id.buttonB);
         downButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +82,8 @@ public class MainActivity extends Activity {
                     mVolume -= 2;
                     tv.setText(String.valueOf(mVolume));
                 }
-
+                mPlayer.setVolume((float) mVolume / mVolumeMax,
+                        (float) mVolume / mVolumeMax);
             }
         });
 
@@ -77,15 +95,23 @@ public class MainActivity extends Activity {
         /**SoundPool es 1 clase para reproducir pequeñas pistas de audio. Con esta clase podemos repetir
          *  la reproducción de sonidos y hasta reproducir múltiples sonidos de manera simultánea.
          *  Recordar que los archivos no deben superar 1MB
-         *  El primer parametro es cunatos strings vamos a manejar en paralelo, en el 2 le indicamos que es
+         *  El primer parametro es cuantos strings vamos a manejar en paralelo, en el 2 le indicamos que es
          *  de tipo MUSIC y el 3 es la calidad, pero no tiene efecto ahora*/
 
-          mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+         //Dejo comentado mSoundPool para utilizar mPlayer
+         // mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
 
         /** Cargamos la cancion, le pasamos 3 parametros en el constructor, this para indicar el contexto sobre donde
          * lo vamos a lanzar (sobre el activity), el 2 param es el recurso id(la cancion), 3 param la prioridad*/
-        mSoundId = mSoundPool.load(this, R.raw.miracle, 1);
+        //mSoundId = mSoundPool.load(this, R.raw.hangout_dingtone, 1);
 
+        /*CARGAMOS LA CANCIÓN NUEVA FORMA - NO HACE FALTA GENERAR UN NUEVO SOUNDPOOL
+        * Con el método estático MediaPlayer create, le pasamos this  para indicarle este activity
+        * y dónde está el Id de la canción. Una vez que lo cree me devolverá un indentificador mPlayer como objeto MediaPlayer*/
+        mPlayer = MediaPlayer.create(this, R.raw.miraclelong);
+
+
+        /*
         // Esperamos a que se cargue la cancion completa
         //comprobar si la carga se ha completado a través de un objeto OnLoadCompleteListener.
         mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
@@ -98,8 +124,20 @@ public class MainActivity extends Activity {
                     Log.d("AUDIO", "Cargada correctamente");
                 }
             }
-        });
+        });*/
 
+        /**Hemos sustituido el setOnLoadCompleteListener por setOnPreparedListener, que lo que implementa
+         * es un Listener de tipo onPrepare*/
+
+        mPlayer.setOnPreparedListener(new OnPreparedListener(){
+            @Override
+            public void onPrepared (MediaPlayer mp){
+                Log.d("AUDIO", "Cargada la cancion");
+                //ponemos el playButton activo
+                playButton.setEnabled(true);
+                }
+        }
+        );
         // Suena la cancion
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,22 +146,31 @@ public class MainActivity extends Activity {
                     playButton.setText("||");
                     sonando=1;
                     if (mCanPlayAudio)
-                        mSoundPool.play(mSoundId, (float) mVolume / mVolumeMax,
-                                (float) mVolume / mVolumeMax, 1, 0, 1.0f);
+                        mPlayer.setVolume((float) mVolume / mVolumeMax,
+                                (float) mVolume / mVolumeMax);
+                    mPlayer.start();
+                    //mSoundPool.play(mSoundId, (float) mVolume / mVolumeMax,
+                    //        (float) mVolume / mVolumeMax, 1, 0, 1.0f);
                 }else if(sonando==1){
                     playButton.setText("Play");
                     sonando=2;
-                    mSoundPool.pause(mSoundId);
+                    mPlayer.setVolume((float) mVolume / mVolumeMax,
+                            (float) mVolume / mVolumeMax);
+                    mPlayer.pause();
+                    //mSoundPool.pause(mSoundId);
                 }else{
                     playButton.setText("||");
                     sonando=1;
-                    mSoundPool.resume(mSoundId);
+                    mPlayer.setVolume((float) mVolume / mVolumeMax,
+                            (float) mVolume / mVolumeMax);
+                    mPlayer.start();
+                    //mSoundPool.resume(mSoundId);
                 }
             }
 
         });
 
-        // Request audio focus - solicitamos el foco de sonido de nuestro aplicativo
+        // Request audio focus
         int result = mAudioManager.requestAudioFocus(afChangeListener,
                 AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
 
@@ -144,9 +191,10 @@ public class MainActivity extends Activity {
     protected void onPause() {
         Log.d("AUDIO", "EN PAUSA");
         if (null != mSoundPool) {
-            mSoundPool.unload(mSoundId);
-            mSoundPool.release();
-            mSoundPool = null;
+            mPlayer.release();
+            //mSoundPool.unload(mSoundId);
+            //mSoundPool.release();
+            //mSoundPool = null;
         }
         mAudioManager.setSpeakerphoneOn(false);
         mAudioManager.unloadSoundEffects();
@@ -154,7 +202,7 @@ public class MainActivity extends Activity {
     }
 
     // Listen for Audio focus changes
-    AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+    OnAudioFocusChangeListener afChangeListener = new OnAudioFocusChangeListener() {
         public void onAudioFocusChange(int focusChange) {
             if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
                 mAudioManager.abandonAudioFocus(afChangeListener);
