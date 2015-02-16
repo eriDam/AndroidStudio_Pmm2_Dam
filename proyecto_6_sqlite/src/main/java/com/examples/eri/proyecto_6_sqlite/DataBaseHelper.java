d1958e72ffb71840ef95872a0503f53376ed1766
eriDam <erika.c.caballero@gmail.com>
2015-02-16 20:47:15 +0100

    Project 6. SQLite II 1- As we have inherited ListActivity class, generated a list in your layout main_activity
    2-Create a list_layout two textviews to be the id and the name of the artist
    
    3-We create a method in DataBaseHelper to read the data and display them in a list (Cursor)

--- proyecto_6_sqlite/src/main/java/com/examples/eri/proyecto_6_sqlite/DataBaseHelper.java
+++ proyecto_6_sqlite/src/main/java/com/examples/eri/proyecto_6_sqlite/DataBaseHelper.java
@@ -2,6 +2,7 @@ package com.examples.eri.proyecto_6_sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -9,15 +10,26 @@ import android.util.Log;
 /**
  * Created by erika_000 on 16/02/2015.
  */
+
+/*Creamos esta clase propia extendida de SQLiteOpenHelper, es una clase especifica, un ayudante para
+* manejar las BDS dentro de SQLite, debemos sobreescribir tanto oncreate como onUpgrade,
+ * estas deben estar definidads como minimo*/
 public class DataBaseHelper extends SQLiteOpenHelper {
-    //Datos de la tabla
+
+    //Datos de la tabla - Defino Atributos
+    //Ponemos el nombre de la Base de datos
     final private static String NAME = "artistas_db";
+    //Indicamos el nombre de la única tabla que tendremos
     final static String TABLE_NAME = "artistas";
 
+    /*Indicamos las columnas, el _id, se define de esta forma, por que para
+     *posteriores lecturas y adaptadores (adapters), se necesita q este este campo,
+     *no se le puede quitar el guión*/
     final static String ID = "_id";
     final static String ARTIST_NAME = "nombre";
 
     //Comandos
+    /**Dentro de un array d Strings, almaceno las columnas*/
     final static String[] columns = { ID, ARTIST_NAME };
     final private static String CREATE_CMD =
 
@@ -36,12 +48,16 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         this.mContext = context;
     }
 
+    //Creación de la base de datos, el onCreate lo utilizaremos tanto para generar la tabla
+    //como para crear/rellenar los campos dentro de la misma, para ello utilizaremos la clase ContentValues
     @Override
     public void onCreate(SQLiteDatabase db) {
         //Creamos la base de datos
         Log.i(this.getClass().toString(), "Tabla ARTISTAS creada");
+        //Generamos la tabla, CREATE_CMD es un string que contiene la sentencia SQL para la creación de la tabla
         db.execSQL(CREATE_CMD);
-        //La rellenamos
+        /*Rellenamos la tabla,  utilizamos la Clase ContentValues, que permite de una forma fácil
+        * con el método put indicamos el campo o campos y el valor y con insert, le idnicamos la tabla */
         ContentValues values = new ContentValues();
 
         values.put(DataBaseHelper.ARTIST_NAME, "Vetusta Morla");
@@ -60,6 +76,15 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    //Método accesible desde fuera para la  lectura de la Base de datos
+    //cursor es una clase de List, new String [] {} es por si queremos pasar mas parametros, en este caso no
+    public Cursor readArtistas(SQLiteDatabase db) {
+        return db.query(TABLE_NAME,
+                columns,null,new String [] {}, null,null,
+                null);
 
     }
 }
--- proyecto_6_sqlite/src/main/java/com/examples/eri/proyecto_6_sqlite/MainActivity.java
+++ proyecto_6_sqlite/src/main/java/com/examples/eri/proyecto_6_sqlite/MainActivity.java
@@ -1,16 +1,21 @@
 package com.examples.eri.proyecto_6_sqlite;
 
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
     private DataBaseHelper mDbHelper;
     private SQLiteDatabase db;
+    private SimpleCursorAdapter mAdapter;
+    private Cursor c;
 
     private static final String TAG = "Datos";
 
@@ -19,10 +24,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Creamos una nueva DataBase
         mDbHelper = new DataBaseHelper(this);
-        db = mDbHelper.getWritableDatabase();
+        db = mDbHelper.getWritableDatabase();//creamos el sqlite através de getWritableDatabase, Sin getWritableDatabase no se genera
         Toast.makeText(getBaseContext(), "Base de datos preparada", Toast.LENGTH_SHORT).show();
+
+        //Leemos la base de datos y mostramos la informacion a través del método creado en
+        // DataBaseHelper readArtistas
+        Cursor c = mDbHelper.readArtistas(db);
+        /*Devuelve un cursor, que se puede introducir en
+        un adaptador, ya que para meter los datos en un list view usamos un adaptador ya generado
+         por android, este necesita que le pasemos una serie de parámetros,
+        *como es el cursor, el layout, el nombre de las columnas(FROM desde donde),
+        * este seria el (TO hacia donde va a ir la informacion) new int[] { R.id._id, R.id.nombre }*/
+        mAdapter = new SimpleCursorAdapter(this, R.layout.list_layout, c,
+                DataBaseHelper.columns, new int[] { R.id._id, R.id.nombre },
+                0);
+        //El mAdapter (los datos), Se lo pasamos al setListAdapter (que sería el visor)
+        setListAdapter(mAdapter);
+
     }
 
 
